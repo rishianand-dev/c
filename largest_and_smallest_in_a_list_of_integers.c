1eb610718455d273ff8860c11a8e4f96f8759764
rishianand39 <rishi.rn818@gmail.com>
2023-12-28 22:55:04 +0530

    largest and smallest in list test case resolved

--- largest_and_smallest_in_a_list_of_integers.c
+++ largest_and_smallest_in_a_list_of_integers.c
@@ -6,15 +6,17 @@ int main(){
     printf("Enter the size of the list: ");
     scanf("%d", &size_of_list);
     for(int i= 0; i< size_of_list; i++){
-        printf("\nEnter element %d: ", i);
-        scanf("%d\n", &list[i]);
+        printf("\nEnter %d th element ", i);
+        scanf("%d", &list[i]);
     }
     minimum = list[0];
     maximum = list[0];
-    for(int i =0; i<size_of_list;i++){
+//    printf("%d %d %d %d %d", list[0], list[1], list[2], list[3], list[4]);
+
+    for(int i =1; i<size_of_list;i++){
         if(list[i]< minimum){
             minimum = list[i];
-        }else{
+        }else if(list[i] > maximum){
             maximum = list[i];
         }
     }
